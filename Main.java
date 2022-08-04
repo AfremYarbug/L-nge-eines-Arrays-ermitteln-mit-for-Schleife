ddddce889478e750f5487eb8c717db8bf4090841
Afrem Yarbug <95577685+AfremYarbug@users.noreply.github.com>
2022-08-04 16:30:45 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,21 +6,26 @@ public class Main {
 
         int[] werte = {2, 4, 7, 9};
 
+        for (int i = werte.length - 1; i >= 0; i--) {
+            System.out.println("Vorwaerts umstaendliche Version: Wert Nr. " + i + ": " + werte[werte.length - 1 - i]);
+        }
+
+        System.out.println("-------------------------------------------------");
+
         for (int i = 0; i < werte.length; i++) {
-            System.out.println("Vorwärts: Wert Nr. " + i + ": " + werte[i]);
+            System.out.println("Vorwaerts: Wert Nr. " + i + ": " + werte[i]);
         }
 
-        System.out.println("---------------------------");
+        System.out.println("-------------------------------------------------");
+
+        for (int i = 0; i < werte.length; i++) {
+            System.out.println("Rueckwaerts umstaendliche Version: Wert Nr. " + i + ": " + werte[werte.length - 1 - i]);
+        }
+
+        System.out.println("-------------------------------------------------");
 
         for (int i = werte.length - 1; i >= 0; i--) {
-            System.out.println("Rueckwärts umstaendliche Version: Wert Nr. " + i + ": " + werte[werte.length - 1 - i]);
+            System.out.println("Rueckwaerts: Wert Nr. " + i + ": " + werte[i]);
         }
-
-        System.out.println("---------------------------");
-
-        for (int i = werte.length - 1; i >= 0; i--) {
-            System.out.println("Rückwärts: Wert Nr. " + i + ": " + werte[i]);
-        }
-
     }
 }
